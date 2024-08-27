ba60499f542c3d518cf8ad1aafb8e115ae915b0d
Linux <s3791361@student.rmit.edu.au>
2024-08-27 13:40:54 +1000

    added description

--- src/tests/unit/model.note.test.js
+++ src/tests/unit/model.note.test.js
@@ -13,11 +13,12 @@ describe('mockingoose', () => {
         it('Validate Model', async () => {
             const todo = new Note({
                 title: "Task Note",
-                description: "" // "This can't be blank"
+                description: "description" // "This can't be blank"
             });
 
             const result = await todo.validateSync();
             expect(result).toBe(undefined);
         });
     })
+})
 })
